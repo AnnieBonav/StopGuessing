2189406048c51efdf443cefa30f1875c9f599d8e
AnnieBonav <anniebonav@outlook.com>
2023-01-16 17:19:34 -0600

    Adds tabs to fragment.

--- StopGuessingM3/app/src/main/java/com/anniebonav/stopguessingm3/BlueprintsFragment.kt
+++ StopGuessingM3/app/src/main/java/com/anniebonav/stopguessingm3/BlueprintsFragment.kt
@@ -11,7 +11,6 @@ import com.anniebonav.stopguessingm3.databinding.FragmentBlueprintsBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class BlueprintsFragment : Fragment() {
-    private lateinit var _bottomNavigationView: BottomNavigationView
     private var _binding: FragmentBlueprintsBinding? = null
     private val binding get() = _binding!! // This property is only valid between onCreateView and onDestroyView.
 
@@ -22,31 +21,7 @@ class BlueprintsFragment : Fragment() {
         val context = activity as MainActivity
         _binding = FragmentBlueprintsBinding.inflate(inflater, container, false)
 
-        _bottomNavigationView = binding.bottomNavigation
-        var selectedId = _bottomNavigationView.selectedItemId
-        Log.d("Selected item", "$selectedId")
 
-        _bottomNavigationView.setOnItemSelectedListener{ item ->
-            when(item.itemId){
-                R.id.home->{
-                    findNavController().navigate(R.id.action_BlueprintsFragment_to_HomeFragment)
-                    true
-                }
-
-                R.id.meals->{
-                    true
-                }
-
-                R.id.ingredients->{
-                    true
-                }
-                else ->{
-                    false
-                }
-            }
-        }
-
-        // Inflate the layout for this fragment
         return binding.root
     }
 }
