8808026acf99401efe3e03ec0f7f0d7fc3006253
AnnieBonav <anniebonav@outlook.com>
2023-01-16 13:47:34 -0600

    Navigation is working with bottom menu.

--- StopGuessingM3/app/src/main/java/com/anniebonav/stopguessingm3/HomeFragment.kt
+++ StopGuessingM3/app/src/main/java/com/anniebonav/stopguessingm3/HomeFragment.kt
@@ -1,36 +1,55 @@
 package com.anniebonav.stopguessingm3
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.anniebonav.stopguessingm3.databinding.FragmentBlueprintsBinding
+import com.anniebonav.stopguessingm3.MainActivity
+import com.anniebonav.stopguessingm3.databinding.FragmentHomeBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class BlueprintsFragment : Fragment() {
+class HomeFragment : Fragment() {
     private lateinit var _bottomNavigationView: BottomNavigationView
-    private var _binding: FragmentBlueprintsBinding? = null
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!! // This property is only valid between onCreateView and onDestroyView.
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         val context = activity as MainActivity
-        _binding = FragmentBlueprintsBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         _bottomNavigationView = binding.bottomNavigation
 
-        _bottomNavigationView.setOnClickListener{ item ->
-            when(item.id){
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.addFab.setOnClickListener {
+            Log.d("Navigation", "I am here")
+            findNavController().navigate(R.id.action_HomeFragment_to_MealPlansFragment)
+        }
+
+        _bottomNavigationView.setOnItemSelectedListener{ item ->
+            Log.d("Navigation", "Inside setOnClick")
+            when(item.itemId){
                 R.id.home->{
-                    findNavController().navigate(R.id.action_BlueprintsFragment_to_HomeFragment)
+                    Log.d("Navigation", "Navigating home")
                     true
                 }
 
                 R.id.meals->{
+                    Log.d("Navigation", "Navigating")
+                    findNavController().navigate(R.id.action_HomeFragment_to_BlueprintsFragment)
                     true
                 }
 
@@ -42,8 +61,5 @@ class BlueprintsFragment : Fragment() {
                 }
             }
         }
-
-        // Inflate the layout for this fragment
-        return binding.root
     }
 }
--- StopGuessingM3/app/src/main/java/com/anniebonav/stopguessingm3/MainActivity.kt
+++ StopGuessingM3/app/src/main/java/com/anniebonav/stopguessingm3/MainActivity.kt
@@ -54,8 +54,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /*
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
+    }*/
 }
